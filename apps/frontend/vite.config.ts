a8fa3e613c03565e203676531ed4ec17d2a1e574
Natalia Ivanov <144643904+nataliaivan0v@users.noreply.github.com>
2025-01-06 14:59:37 -0500

    change to 4000

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   cacheDir: '../../node_modules/.vite/apps/frontend',
 
   server: {
-    port: 4200,
+    port: 4000,
     host: 'localhost',
   },
 
   preview: {
-    port: 4300,
+    port: 4000,
     host: 'localhost',
   },
 
